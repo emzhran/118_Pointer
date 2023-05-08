f1c723c6cb5fbd2d2878cb43accb5b63c6ef8d67
emzhran <zachran750@gmail.com>
2023-05-08 11:00:33 +0700

    tambahan int main

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -8,3 +8,17 @@ class mahasiswa {
 		cout << "No Induk = " << nim << endl;
 	}
 };
+
+int main() {
+	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa& refMhs = mhs;
+	refMhs.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	mhs.showNim();
+	return 0;
+}
